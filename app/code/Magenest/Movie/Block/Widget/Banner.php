79e7c797643ca68740e0d93931fcd44fb1f7ec06
secret-star1202 <starDev1202@outlook.com>
2023-05-04 16:10:04 +0700

    create customer group

--- app/code/Magenest/Movie/Block/Widget/Banner.php
+++ app/code/Magenest/Movie/Block/Widget/Banner.php
@@ -1,10 +1,35 @@
 <?php
 
 namespace Magenest\Movie\Block\Widget;
+
+use Magento\Customer\Model\Group;
+use Magento\Customer\Model\Session;
+use Magento\Framework\App\Http\Context;
 use Magento\Framework\View\Element\Template;
 use Magento\Widget\Block\BlockInterface;
 
 class Banner extends Template implements BlockInterface
 {
     protected $_template = "widget/banner.phtml";
+    protected $_customerSession;
+
+    protected $httpContext;
+    protected $_customerGroupCollection;
+
+    public function __construct(Template\Context $context, Session $customerSession, Group $customerGroupCollection, Context $httpContext, array $data = [])
+    {
+        $this->httpContext = $httpContext;
+        $this->_customerSession = $customerSession;
+        $this->_customerGroupCollection = $customerGroupCollection;
+        parent::__construct($context, $data);
+    }
+
+    public function getCustomerGroup()
+    {
+        $isLogged = $this->httpContext->getValue(\Magento\Customer\Model\Context::CONTEXT_AUTH);
+        $customerGroupId = $this->httpContext->getValue(\Magento\Customer\Model\Context::CONTEXT_GROUP);
+        $groupCollection = $this->_customerGroupCollection->load($customerGroupId);
+        return $groupCollection->getCustomerGroupCode();
+
+    }
 }
