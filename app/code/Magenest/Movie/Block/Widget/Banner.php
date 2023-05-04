816ec8e551d2149186f5b4792742d90d54eba560
secret-star1202 <starDev1202@outlook.com>
2023-05-04 11:08:28 +0700

    create widget banner

---
+++ app/code/Magenest/Movie/Block/Widget/Banner.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Magenest\Movie\Block\Widget;
+use Magento\Framework\View\Element\Template;
+use Magento\Widget\Block\BlockInterface;
+
+class Banner extends Template implements BlockInterface
+{
+    protected $_template = "widget/banner.phtml";
+}
