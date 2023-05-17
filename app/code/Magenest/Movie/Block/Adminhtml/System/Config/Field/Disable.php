29c3f22743ab710f538e68806e9632e11929a980
secret-star1202 <starDev1202@outlook.com>
2023-05-17 14:01:46 +0700

    create buy now

---
+++ app/code/Magenest/Movie/Block/Adminhtml/System/Config/Field/Disable.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace Magenest\Movie\Block\Adminhtml\System\Config\Field;
+
+use Magento\Config\Block\System\Config\Form\Field;
+use Magento\Framework\Data\Form\Element\AbstractElement;
+
+class Disable extends Field
+{
+    /**
+     * @param AbstractElement $element
+     * @return string
+     */
+    protected function _getElementHtml(AbstractElement $element)
+    {
+        $element->setDisabled('disabled');
+        return $element->getElementHtml();
+    }
+}
---
+++ app/code/Magenest/Movie/Controller/Cart/Add.php
@@ -0,0 +1,104 @@
+<?php
+
+/**
+ * Intern
+ * Copyright (C) 2020 Intern <info@Intern.com>
+ *
+ * @package Intern_BuyNow
+ * @copyright Copyright (c) 2020 Intern (http://www.Intern.com/)
+ * @license http://opensource.org/licenses/gpl-3.0.html GNU General Public License,version 3 (GPL-3.0)
+ * @author Intern <info@Intern.com>
+ */
+
+namespace Magenest\Movie\Controller\Cart;
+
+class Add extends \Magento\Checkout\Controller\Cart\Add
+{
+    /**
+     * Add product to shopping cart action
+     *
+     * @return \Magento\Framework\App\ResponseInterface|\Magento\Framework\Controller\ResultInterface
+     * @SuppressWarnings(PHPMD.CyclomaticComplexity)
+     */
+    public function execute()
+    {
+        if (!$this->_formKeyValidator->validate($this->getRequest())) {
+            $this->messageManager->addErrorMessage(
+                __('Your session has expired')
+            );
+            return $this->resultRedirectFactory->create()->setPath('*/*/');
+        }
+
+        $params = $this->getRequest()->getParams();
+
+        try {
+            if (isset($params['qty'])) {
+                $filter = new \Zend_Filter_LocalizedToNormalized(
+                    ['locale' => $this->_objectManager->get(
+                        \Magento\Framework\Locale\ResolverInterface::class
+                    )->getLocale()]
+                );
+                $params['qty'] = $filter->filter($params['qty']);
+            }
+
+            $product = $this->_initProduct();
+            $related = $this->getRequest()->getParam('related_product');
+
+            /**
+             * Check product availability
+             */
+            if (!$product) {
+                return $this->goBack();
+            }
+
+            $buyNowHelper = $this->_objectManager->create(\Magenest\Movie\Helper\Data::class);
+            $cartProducts = $buyNowHelper->keepCartProducts();
+            if (!$cartProducts) {
+                $this->cart->truncate(); //remove all products from cart
+            }
+
+            $this->cart->addProduct($product, $params);
+            if (!empty($related)) {
+                $this->cart->addProductsByIds(explode(',', $related));
+            }
+
+            $this->cart->save();
+
+            /**
+             * @todo remove wishlist observer \Magento\Wishlist\Observer\AddToCart
+             */
+            $this->_eventManager->dispatch(
+                'checkout_cart_add_product_complete',
+                ['product' => $product, 'request' => $this->getRequest(), 'response' => $this->getResponse()]
+            );
+
+            if (!$this->_checkoutSession->getNoCartRedirect(true)) {
+                $baseUrl = $this->_url->getBaseUrl();
+                return $this->goBack($baseUrl . 'checkout/', $product);
+            }
+        } catch (\Magento\Framework\Exception\LocalizedException $e) {
+            if ($this->_checkoutSession->getUseNotice(true)) {
+                $this->messageManager->addNoticeMessage(
+                    $this->_objectManager->get(\Magento\Framework\Escaper::class)->escapeHtml($e->getMessage())
+                );
+            } else {
+                $messages = array_unique(explode("\n", $e->getMessage()));
+                foreach ($messages as $message) {
+                    $this->messageManager->addErrorMessage(
+                        $this->_objectManager->get(\Magento\Framework\Escaper::class)->escapeHtml($message)
+                    );
+                }
+            }
+            $url = $this->_checkoutSession->getRedirectUrl(true);
+            if (!$url) {
+                $cartUrl = $this->_objectManager->get(\Magento\Checkout\Helper\Cart::class)->getCartUrl();
+                $url = $this->_redirect->getRedirectUrl($cartUrl);
+            }
+            return $this->goBack($url);
+        } catch (\Exception $e) {
+            $this->messageManager->addException($e, __('We can\'t add this item to your shopping cart right now.'));
+            $this->_objectManager->get(\Psr\Log\LoggerInterface::class)->critical($e);
+            return $this->goBack();
+        }
+    }
+}
