4e79270f3b158cea4a071ecad0fc147d504a02c5
secret-star1202 <starDev1202@outlook.com>
2023-05-26 08:54:15 +0700

    update code

--- app/code/Magenest/Movie/Block/Blog.php
+++ app/code/Magenest/Movie/Block/Blog.php
@@ -2,6 +2,7 @@
 
 namespace Magenest\Movie\Block;
 
+use Magenest\Movie\Model\BlogFactory;
 use Magenest\Movie\Model\ResourceModel\Blog\CollectionFactory as BlogCollectionFactory;
 use Magento\Framework\View\Element\Template;
 
@@ -10,14 +11,17 @@ class Blog extends Template
     protected $_blogCollectionFactory;
     protected $_resource;
 
+    protected $_blogFactory;
+
     public function __construct(
         Template\Context                          $context,
         BlogCollectionFactory                     $blogCollectionFactory,
         \Magento\Framework\App\ResourceConnection $Resource,
+        BlogFactory                               $blogFactory
     ) {
         $this->_blogCollectionFactory = $blogCollectionFactory;
         $this->_resource = $Resource;
-
+        $this->_blogFactory = $blogFactory;
         parent::__construct($context);
     }
 
@@ -32,12 +36,13 @@ class Blog extends Template
         $collection = $this->_blogCollectionFactory->create();
 //        return $collection->addFieldToSelect('*')->setPageSize(10);
         $admin_user = $this->_resource->getTableName('admin_user');
-
+        $getBlog = $this->_blogFactory->create();
         $collection->getSelect()->joinLeft(
             ['second' => $admin_user],
             'main_table.author_id = second.user_id',
-            ['admin_user_username' => 'second.username', 'admin_user_email' => 'second.email' ]
+            ['admin_user_username' => 'second.username', 'admin_user_email' => 'second.email']
         );
-        return $collection->addFieldToSelect('*')->setPageSize(10);
+//        return $collection->addFieldToSelect('*');
+            return $collection->addFieldToFilter('id', ['gt' => 15]);
     }
 }
--- app/code/Magenest/Movie/Controller/Index/Index.php
+++ app/code/Magenest/Movie/Controller/Index/Index.php
@@ -3,6 +3,7 @@
 namespace Magenest\Movie\Controller\Index;
 
 use Magenest\Movie\Model\DirectorFactory;
+use Magenest\Movie\Model\ResourceModel\Director\CollectionFactory as DirectorCollectionFactory;
 use Magento\Framework\App\Action\Action;
 use Magento\Framework\App\Action\Context;
 use Magento\Framework\View\Result\PageFactory;
@@ -12,24 +13,36 @@ class Index extends Action
     protected $_pageFactory;
     protected $_directorFactory;
 
-    public function __construct(Context $context, PageFactory $pageFactory, DirectorFactory $directorFactory)
+    protected $_directorCollectionFactory;
+    public function __construct(Context $context, PageFactory $pageFactory, DirectorFactory $directorFactory, DirectorCollectionFactory $directorCollectionFactory)
     {
         $this->_pageFactory = $pageFactory;
         $this->_directorFactory = $directorFactory;
+        $this->_directorCollectionFactory = $directorCollectionFactory;
         return parent::__construct($context);
     }
 
     public function execute()
     {
         // TODO: Implement execute() method.
-        $director = $this->_directorFactory->create();
-        $collection = $director->getCollection();
-        foreach ($collection as $item) {
+//        $director = $this->_directorFactory->create();
+        $director = $this->_directorCollectionFactory->create();
+        $director->addFieldToFilter('director_id', ['gt' => 100]);
+//        $director->addFieldToFilter('director_id', ['gt' => 0])->addFieldToFilter(
+//            'name',
+//            [
+//                    ['like' => 'Liam%'],
+//                    ['like' => 'Ryan%']
+//            ]
+//        );
+        foreach ($director as $item) {
             echo "<pre>";
             print_r($item->getData());
             echo "</pre>";
         }
-        exit();
-        return $this->_pageFactory->create();
+//        return $director->addFieldToFilter('id', ['gt' => 15]);
+//        exit();
+//        print_r(json_encode($director->getData()));
+//        return $this->_pageFactory->create();
     }
 }
